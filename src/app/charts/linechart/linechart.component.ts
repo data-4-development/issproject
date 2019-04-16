85de167bd07d6afd7982fd34f3efe56e01f7837f
chaymafourati <chaymafourati12@gmail.com>
2019-04-16 15:13:23 +0200

    commit

--- src/app/charts/linechart/linechart.component.ts
+++ src/app/charts/linechart/linechart.component.ts
@@ -25,18 +25,18 @@ export class LinechartComponent implements OnInit {
           data: [9, 7, 3, 5, 2, 10, 15, 16, 19, 3, 1, 9],
           fill: false,
           lineTension: 0.2,
-          backgroundColor: "pink",
-          borderColor: "pink",
+          backgroundColor: "#87CEEB",
+          borderColor: "#87CEEB",
           borderWidth: 3
         },
         {
           type: 'line',
-          label: 'Unemployment rate in tunisia 2 ',
+          label: 'Inflation Rate in Tunisia ',
           data: [2, 17, 13, 5, 12, 10, 12, 13, 15, 10, 8, 11],
           fill: false,
           lineTension: 0.2,
-          backgroundColor: "green",
-          borderColor: "green",
+          backgroundColor: "#4B0082",
+          borderColor: "#4B0082",
           borderWidth: 2
         }]
       },
@@ -80,16 +80,16 @@ export class LinechartComponent implements OnInit {
   // new Dataset("haja",[12,2, 13, 15, 2, 10, 12, 14, 16, 10, 14, 11]),
   // new Dataset("inflation",[2, 17, 13, 5, 3, 9, 7, 15, 15, 10, 8, 1])
   //]
-  datasets: Datas[] = [{ id: 0, name: "haja", data: [2, 17, 13, 5, 3, 9, 7, 15, 15, 10, 8, 1] },
-  { id: 1, name: "hajtin", data: [12, 5, 7, 15, 13, 19, 9, 5, 4, 7, 14, 1] },
-  { id: 2, name: "haj 3", data: [15, 11, 8, 9, 13, 15, 11, 9, 7, 5, 8, 11]}
+  datasets: Datas[] = [{ id: 0, name: "Data1", data: [2, 17, 13, 5, 3, 9, 7, 15, 15, 10, 8, 1] },
+  { id: 1, name: "Data2", data: [12, 5, 7, 15, 13, 19, 9, 5, 4, 7, 14, 1] },
+  { id: 2, name: "Data3", data: [15, 11, 8, 9, 13, 15, 11, 9, 7, 5, 8, 11]}
   ];
 
-  color:string[]=["#FF0000","#4169E1","#FFD700","#9932CC","#00FF00","#40E0D0"];
+  color:string[]=["#DDA0DD","#F0E68C","#FF0000","#4169E1","#FFD700","#9932CC","#00FF00","#40E0D0"];
   adddata(x, y) {
     this.LineChart.data.datasets.push({
       type: x,
-      label: 'v rate in tunisia ',
+      label: this.datasets[y].name,
       data: this.datasets[y].data,
       fill: false,
       lineTension: 0.2,
