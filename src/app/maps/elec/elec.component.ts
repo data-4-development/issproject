1a7b12ba359b00842a66c6587da559fb8ddf59c6
chaymafourati <chaymafourati12@gmail.com>
2019-05-12 13:05:14 +0200

    vbn,

--- src/app/maps/elec/elec.component.ts
+++ src/app/maps/elec/elec.component.ts
@@ -10,6 +10,15 @@ export class ElecComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+    var divElement = document.getElementById('viz1557656540188'); 
+
+    var vizElement = divElement.getElementsByTagName('object')[0]; 
+    vizElement.style.width = '900px'; 
+    vizElement.style.height = '1050px'; 
+    var scriptElement = document.createElement('script'); scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js'; 
+    vizElement.parentNode.insertBefore(scriptElement, vizElement);
+
   }
 
 }
