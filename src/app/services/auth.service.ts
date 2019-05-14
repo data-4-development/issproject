33525649d58eff15333b70d03024fa6486916ed0
chaymafourati <chaymafourati12@gmail.com>
2019-05-14 02:08:57 +0200

    login/logout

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -22,4 +22,24 @@ export class AuthService {
     return this.http.post('http://localhost:3000/users/register', user, {headers: headers})
      .map(res => res);
   }
+
+  authenticateUser(user){
+    let headers = new HttpHeaders();
+    headers.append('Content-Type','application/json');
+    return this.http.post('http://localhost:3000/users/authenticate', user, {headers: headers})
+     .map(res => res);
+  }
+
+  storeUserData(token, user){
+    localStorage.setItem('id_token', token);
+    localStorage.setItem('user',JSON.stringify(user));
+    this.authToken=token;
+    this.user=user;
+  }
+
+  logout(){
+    this.authToken = null;
+    this.user =null;
+    localStorage.clear();
+  }
 }
